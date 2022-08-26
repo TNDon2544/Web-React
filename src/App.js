bd952f2d02e3e27c93d24758e6725c7ed0716ff2
TNDon2544 <amgod2544@gmail.com>
2022-08-26 10:50:22 +0700

    add func-components.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Header, Content, Footer } from './func-components';
+
+function App() {
+  return(
+    <>
+      <Header/>
+      <Content/>
+      <Footer/>
+    </>
+  )
+ 
+}
+
+export default App;
